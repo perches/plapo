6de61a5420ddd18084258a6599d08652d98106f2
tachibanayu24 <tachibanayu24@gmail.com>
2019-02-03 20:55:11 +0900

    Headerのmockの実装

---
+++ plapo/frontend/src/Components/home/MainContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../common/Header'
+
+class MainContainer extends React.Component {
+  render(){
+    return(
+      <>
+      <Header />
+      </>
+    )
+  }
+}
+
+export default MainContainer
